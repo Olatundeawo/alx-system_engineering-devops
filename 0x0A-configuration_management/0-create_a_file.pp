dbc629869931ce6633c0cb8d1da2337eb9f0439b
Olatundeawo <awotimilehin.babatunde>
2023-03-24 12:57:58 -0700

    modify

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # creating a file
 file  {  '/tmp/school':
-  mode     => '0744',
-  owner    => 'www-data',
-  group    =>  'www-data',
-  content  =>  'I Love Puppet'
-} 
+  mode    => '0744',
+  owner   => 'www-data',
+  group   =>  'www-data',
+  content =>  'I Love Puppet'
+}
