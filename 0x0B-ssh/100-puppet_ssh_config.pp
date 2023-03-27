4f67975ae17b475a63944d17af41b2e23302c853
Olatundeawo <awotimilehin.babatunde>
2023-03-26 22:46:37 -0700

    modify

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 #this manifest changes the ssh configuartion
 exec {'/etc/ssh/ssh_config':
 path     => '/bin',
-command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication 		       no" >> /etc/ssh/ssh_config',
 provider => 'shell',
 }
