95156f0ce2d96e142dc7f257103f08c8a2e8dc54
Olatundeawo <awotimilehin.babatunde>
2023-04-07 06:03:39 -0700

    modify

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
 # configure my server with puppet
-exect {"Configuring server":
+exec { 'Configuring server':
   provider => shell,
   command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; sudo sed -i s/listen\s*80;/listen 80 default_server;/g /etc/nginx/sites-available/default echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
