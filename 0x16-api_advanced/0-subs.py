dd222c91f64f1dd6b3272c05fb998a39aa420a3b
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-05-10 15:21:46 +0000

    mdify

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-import requests
 """ a script that has a method that returns the 
     number of subscribers
 """
+import requests
+import sys
+
+
 def number_of_subscribers(subreddit):
     """ a methos that returns the number of a subscribers """
-    headers = {"User-Agent": "chrome 5.8"}
+    headers = {"User-Agent": "xica369"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url,headers=headers, allow_redirects=False)
     if response.status_code == 200:
