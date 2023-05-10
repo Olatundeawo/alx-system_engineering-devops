04761f7d2d9f09516e710a668b25ec72682e8763
Olatundeawo <awotimilehin.babatunde@gmail.com>
2023-05-10 15:25:47 +0000

    modify

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,6 +11,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "xica369"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url,headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         return (response.json().get('data').get('subscribers'))
     else:
